d35fc25282cfaa28d87f875d47c1e55f7fd3d39d
Karan Reddy <kreddy@hsfts.com>
2022-11-18 08:13:23 -0800

    switching to token from code in response

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function IndexPage() {
   const router = useRouter();
   useEffect(() => {
     
-    window.location.replace('https://rcadmin1.auth.us-west-2.amazoncognito.com/login?client_id=63040hgvklso6cfdotcd651jah&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://manager-login-app.vercel.app/agent/');
+    window.location.replace('https://rcadmin1.auth.us-west-2.amazoncognito.com/login?client_id=63040hgvklso6cfdotcd651jah&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://manager-login-app.vercel.app/agent/');
   })
   
   // const [isLoggedin, setIsLoggedin] = useState(false);
